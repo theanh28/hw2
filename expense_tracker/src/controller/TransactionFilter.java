1404651515055ade9ec516be252c7fd725a751a1
ThomasN12 <phuocthinhdn12023@gmail.com>
2023-10-20 14:57:59 -0400

    update file locations

--- expense_tracker/src/controller/TransactionFilter.java
+++ expense_tracker/src/controller/TransactionFilter.java
@@ -1,6 +1,8 @@
-package model;
+package controller;
 
 import java.util.List;
+
+import model.Transaction;
 
 public interface TransactionFilter {
   List<Transaction> filter(List<Transaction> transactions);
