cfb29e5c482faf2073f89f763b83eab3f5df8afe
ThomasN12 <phuocthinhdn12023@gmail.com>
2023-10-20 19:14:16 -0400

    add jdoc comment for interface

--- expense_tracker/src/controller/TransactionFilter.java
+++ expense_tracker/src/controller/TransactionFilter.java
@@ -4,6 +4,15 @@ import java.util.List;
 
 import model.Transaction;
 
+/**
+ * Represents a filter strategy for transactions.
+ */
 public interface TransactionFilter {
+  /**
+   * Filters the given list of transactions based on specific criteria.
+   *
+   * @param transactions The list of transactions to be filtered.
+   * @return A list of transactions that match the filter criteria.
+   */
   List<Transaction> filter(List<Transaction> transactions);
 }
