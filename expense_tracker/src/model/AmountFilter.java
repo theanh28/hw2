88b61f7f5dee7fde157e2a732188432ed8d1cacf
ThomasN12 <phuocthinhdn12023@gmail.com>
2023-10-19 23:33:41 -0400

    add javadoc for filter

--- expense_tracker/src/model/AmountFilter.java
+++ expense_tracker/src/model/AmountFilter.java
@@ -3,13 +3,26 @@ package model;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * This class represents a filter based on the transaction amount.
+ */
 public class AmountFilter implements TransactionFilter {
   private double amount;
 
+  /**
+   * Constructs a new AmountFilter with the specified amount value.
+   *
+   * @param amount the amount value to be used for filtering
+   */
   public AmountFilter(double amount) {
     this.amount = amount;
   }
 
+  /**
+   * Filters the list of transactions based on the amount value.
+   * @param transactions the list of transactions to be filtered
+   * @return a list of transactions that match the specified amount
+   */
   @Override
   public List<Transaction> filter(List<Transaction> transactions) {
     return transactions.stream()
--- expense_tracker/src/model/CategoryFilter.java
+++ expense_tracker/src/model/CategoryFilter.java
@@ -3,16 +3,29 @@ package model;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * This class represents a filter based on the transaction category.
+ */
 public class CategoryFilter implements TransactionFilter {
   private String category;
 
+  /**
+   * Constructs a new CategoryFilter with the specified category value.
+   *
+   * @param category the category value to be used for filtering
+   */
   public CategoryFilter(String category) {
     this.category = category;
   }
 
+  /**
+   * Filters the list of transactions based on the category value.
+   * @param transactions the list of transactions to be filtered
+   * @return a list of transactions that match the specified category
+   */
   @Override
   public List<Transaction> filter(List<Transaction> transactions) {
-    return transactions.stream()
+      return transactions.stream()
       .filter(transaction -> transaction.getCategory().equals(category))
       .collect(Collectors.toList());
   }
